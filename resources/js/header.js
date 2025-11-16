a26ca18e11778b9cb5d38d6b11086c7813d1a4bd
Jan Louie Salimbago <salimbagojanlouie02@gmail.com>
2025-11-16 12:42:52 +0800

    side-bar-and-header-responsiveness

--- resources/js/header.js
+++ resources/js/header.js
@@ -9,3 +9,49 @@ profileImageCon.addEventListener('click', () => {
         linksCon.classList.add('links_active');
     }
 });
+
+
+// Sidebar responsive
+// Sidebar responsive
+
+const toggleBtn = document.getElementById("toggleSidebar");
+const sidebar = document.querySelector(".menu-bar");
+const overlay = document.getElementById("sidebarOverlay");
+const closeBtn = document.getElementById("closeSidebar");
+
+// Apply saved state BEFORE browser paints (fix flicker)
+(function(){
+  const sidebarState = localStorage.getItem("sidebarState");
+  if(sidebarState === "collapsed" && window.innerWidth > 992){
+    sidebar.classList.add("collapsed");
+  }
+})();
+
+toggleBtn.addEventListener("click", () => {
+  if(window.innerWidth  >= 992){
+
+    sidebar.classList.toggle("collapsed");
+
+    // save state
+    if(sidebar.classList.contains("collapsed")){
+      localStorage.setItem("sidebarState", "collapsed");
+    }else{
+      localStorage.setItem("sidebarState", "expand");
+    }
+  }else{
+    // Medium/Small slider sidebar
+    sidebar.classList.add("show");
+    overlay.classList.add("active");
+  }
+});
+// Close Button
+closeBtn.addEventListener("click",() =>{
+  sidebar.classList.remove("show");
+  overlay.classList.remove("active");
+});
+// close outside the sidebar
+
+overlay.addEventListener("click", () =>{
+  sidebar.classList.remove("show");
+  overlay.classList.remove("active")
+});
