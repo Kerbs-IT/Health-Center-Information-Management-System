7196fb5a0174e967a690f59c9cf3664391292cc8
Jan Louie Salimbago <salimbagojanlouie02@gmail.com>
2025-12-05 10:51:21 +0800

    record-vaccination-responsiveness

--- resources/views/records/vaccination/editPatientDetails.blade.php
+++ resources/views/records/vaccination/editPatientDetails.blade.php
@@ -20,7 +20,7 @@
     'resources/css/patient/record.css',
     'resources/js/patient/editPatientDetails.js'])
     @include('sweetalert::alert')
-    <div class="patient-details vh-100 d-flex">
+    <div class="patient-details min-vh-100 d-flex">
         <aside>
             @include('layout.menuBar')
         </aside>
@@ -30,7 +30,7 @@
                 <main class="flex-column p-2">
                     <div class="top-part d-flex justify-content-between px-2 align-items-center border-bottom">
                         <h2>Update Patient Details</h2>
-                        <div class="sequence-links d-flex justify-content-center align-items-center">
+                        <div class="sequence-links d-none d-md-flex justify-content-center align-items-center">
                             <h5 class="mb-0 text-muted cursor-pointer fw-normal">Records</h5>
                             <svg xmlns="http://www.w3.org/2000/svg" class="arrow-right" viewBox="0 0 320 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
                                 <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
@@ -43,7 +43,7 @@
                         </div>
                     </div>
                     <!-- main content -->
-                    <div class="flex-grow-1 py-3 px-5 bg-white mx-3 mt-2 rounded">
+                    <div class="flex-grow-1 py-3 px-1 px-md-5 bg-white mx-md-3 mx-0 mt-2 rounded">
                         <a href="{{route('record.vaccination')}}" class="btn btn-danger px-4 fs-5 mb-3">Back</a>
                         <form action="" method="post" class="d-flex flex-column align-items-center  justify-content-center rounded overflow-hidden" id="update-form">
                             @method('PUT')
@@ -51,31 +51,31 @@
                             <div class="step d-flex flex-column w-100 rounded  px-2">
                                 <div class="info">
                                     <h4>Personal Info</h4>
-                                    <div class="mb-2 d-flex gap-1">
-                                        <div class="input-field w-50">
+                                    <div class="mb-md-2 mb-0 d-flex gap-1 flex-wrap flex-column flex-md-row">
+                                        <div class="input-field flex-fill">
                                             <input type="text" id="first_name" placeholder="First Name" class="form-control" name="first_name" value="{{$info->first_name}}">
                                             @error('first_name')
                                             <small class="text-danger">{{$message}}</small>
                                             @enderror
 
                                         </div>
-                                        <div class="input-field w-50">
+                                        <div class="input-field flex-fill">
                                             <input type="text" id="middle_initial" placeholder="Middle Initial" class="form-control" name="middle_initial" value="{{$info->middle_initial}}">
                                             @error('middle_initial')
                                             <small class="text-danger">{{$message}}</small>
                                             @enderror
 
                                         </div>
-                                        <div class="input-field w-50">
+                                        <div class="input-field flex-fill">
                                             <input type="text" id="last_name" placeholder="Last Name" class="form-control" name="last_name" value="{{$info->last_name}}">
                                             @error('last_name')
                                             <small class="text-danger">{{$message}}</small>
                                             @enderror
                                         </div>
                                     </div>
-                                    <div class="mb-2 d-flex gap-1">
+                                    <div class="mb-md-2 mb-0 d-flex gap-1 flex-wrap flex-xl-nowrap flex-column flex-md-row">
                                         <!-- date of birth -->
-                                        <div class="input-field w-50">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="birthdate">Date of Birth</label>
                                             <input type="date" id="birthdate" placeholder="20" class="form-control w-100 px-5" name="date_of_birth" value="{{optional($info)->date_of_birth?? ''}}">
                                             @error('date_of_birth')
@@ -83,7 +83,7 @@
                                             @enderror
                                         </div>
                                         <!-- place of birth -->
-                                        <div class="input-field w-50">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="place_of_birth">Place of Birth</label>
                                             <input type="text" id="place_of_birth" placeholder="20" class="form-control" name="place_of_birth" value="{{optional($info)-> place_of_birth ?? 'none'}}">
                                             @error('place_of_birth')
@@ -92,7 +92,7 @@
                                         </div>
 
                                         <!-- age -->
-                                        <div class="input-field w-50">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="age">Age</label>
                                             <input type="text" id="age" placeholder="20" class="form-control" name="age" value="{{optional($info)-> age ?? 'none'}}">
                                             @error('age')
@@ -100,8 +100,8 @@
                                             @enderror
                                         </div>
                                     </div>
-                                    <div class="mb-2 d-flex gap-1">
-                                        <div class="input-field w-50">
+                                    <div class="mb-md-2 mb-0 d-flex gap-1 flex-wrap  flex-xl-nowrap flex-column flex-md-row">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="sex">Sex</label>
                                             <div class="input-field d-flex align-items-center p-2">
                                                 <div class="sex-input d-flex align-items-center justify-content-center w-100 gap-1">
@@ -114,14 +114,14 @@
                                             </div>
                                         </div>
                                         <!-- contact -->
-                                        <div class="input-field w-50">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="contact_number" class="">Contact Number</label>
                                             <input type="number" placeholder="+63-936-627-8671" class="form-control" name="contact_number" value="{{optional($info)-> contact_number ?? 'none'}}">
                                             @error('contact_number')
                                             <small class="text-danger">{{$message}}</small>
                                             @enderror
                                         </div>
-                                        <div class="input-field w-50">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="nationality" class="">Nationality</label>
                                             <input type="text" placeholder="ex. Filipino" class="form-control" name="nationality" value="{{optional($info)-> nationality ?? 'none'}}">
                                             @error('nationality')
@@ -129,8 +129,8 @@
                                             @enderror
                                         </div>
                                     </div>
-                                    <div class="mb-2 d-flex gap-1">
-                                        <div class="input-field w-50">
+                                    <div class="mb-md-2 mb-0 d-flex gap-1 flex-wrap flex-xl-nowrap flex-column flex-md-row">
+                                        <div class="input-field xl:w-[50%] flex-fill">
                                             <label for="dateOfRegistration">Date of Registration</label>
                                             <input type="date" id="dateOfRegistration" placeholder="20" class="form-control text-center w-100 px-5 " name="date_of_registration" value="{{optional($info)-> created_at?->format('Y-m-d') ?? ''}}">
                                             @error('date_of_birth')
@@ -138,7 +138,7 @@
                                             @enderror
                                         </div>
                                         <!-- administered by -->
-                                        <div class="mb-2 w-50">
+                                        <div class="mb-md-2 mb-0 xl:w-[50%] flex-fill">
                                             <label for="healthWorkersDropDown">Handled by*</label>
                                             <select name="handled_by" id="healthWorkersDropDown" class="form-select" data-bs-selected-Health-Worker="{{optional($info->medical_record_case[0]->vaccination_medical_record)->health_worker_id ?? 'N/A'}}">
                                                 <option value="" selected disabled>Select a person</option>
@@ -149,8 +149,8 @@
                                         </div>
                                     </div>
                                     <!-- mother name and father -->
-                                    <div class="mb-2 d-flex gap-1">
-                                        <div class="input-field w-50">
+                                    <div class="mb-md-2 mb-0 d-flex gap-1 flex-wrap flex-column flex-md-row">
+                                        <div class="input-field flex-fill">
                                             <label for="motherName">Mother Name</label>
                                             <input type="text" id="mother_name" placeholder="mother name" class="form-control" name="mother_name" value="{{optional($info->medical_record_case[0]->vaccination_medical_record)->mother_name ?? 'N/A'}}">
                                             @error('mother_name')
@@ -158,7 +158,7 @@
                                             @enderror
 
                                         </div>
-                                        <div class="input-field w-50">
+                                        <div class="input-field flex-fill">
                                             <label for="fatherName">Father Name</label>
                                             <input type="text" id="fatherName" placeholder="Father Name" class="form-control" name="father_name" value="{{optional($info->medical_record_case[0]->vaccination_medical_record)->father_name ?? 'N/A'}}">
                                             @error('middle_initial')
@@ -167,17 +167,17 @@
                                         </div>
                                     </div>
                                     <!-- address -->
-                                    <div class="mb-2 d-flex gap-1 flex-column">
+                                    <div class="mb-md-2 mb-0 d-flex gap-1 flex-column">
                                         <h4>Address</h4>
-                                        <div class="input-field d-flex gap-2 align-items-center">
-                                            <div class=" mb-2 w-50">
+                                        <div class="input-field d-flex gap-2 flex-wrap flex-xl-nowrap flex-column flex-md-row w-100">
+                                            <div class=" mb-md-2 mb-0 xl:w-[50%] flex-fill ">
                                                 <label for="street">Street*</label>
-                                                <input type="text" id="street" placeholder="Blk & Lot n Street" class="form-control py-2" name="street" value="{{$street??'none'}}">
+                                                <input type="text" id="street" placeholder="Blk & Lot n Street" class="form-control py-2 text-wrap" name="street" value="{{$street??'none'}}">
                                                 @error('street')
                                                 <small class="text-danger">{{$message}}</small>
                                                 @enderror
                                             </div>
-                                            <div class="mb-2 w-50">
+                                            <div class="mb-md-2 mb-0 xl:w-[50%] flex-fill">
                                                 <label for="brgy">Barangay*</label>
                                                 <select name="brgy" id="brgy" class="form-select py-2" data-bs-purok="{{optional($address)->purok??'none'}}">
                                                     <option value="" selected disabled>Select a brgy</option>
@@ -189,13 +189,13 @@
                                         </div>
                                     </div>
                                     <!-- vital sign -->
-                                    <div class="vital-sign w-100">
-                                        <div class="mb-2 input-field d-flex gap-3 w-100 third-row">
-                                            <div class="mb-2 w-50">
+                                    <div class="vital-sign w-100 flex-wrap flex-column flex-md-row">
+                                        <div class="mb-md-2 mb-0 input-field d-flex gap-3 w-100 third-row flex-column flex-md-row">
+                                            <div class="mb-md-2 mb-0 xl:w-[50%] flex-fill">
                                                 <label for="BP">Birth Height(cm):</label>
                                                 <input type="number" class="form-control w-100" placeholder="00.00" name="vaccination_height" value="{{optional($info->medical_record_case[0]->vaccination_medical_record)->birth_height ?? 'N/A'}}">
                                             </div>
-                                            <div class="mb-2 w-50">
+                                            <div class="mb-md-2 mb-0 xl:w-[50%] flex-fill">
                                                 <label for="BP">Birth Weight(kg):</label>
                                                 <input type="text" class="form-control w-100" placeholder=" 00.00" name="vaccination_weight" value="{{optional($info->medical_record_case[0]->vaccination_medical_record)->birth_weight ?? 'N/A'}}">
                                             </div>
--- resources/views/records/vaccination/viewComponent/viewCase.blade.php
+++ resources/views/records/vaccination/viewComponent/viewCase.blade.php
@@ -1,37 +1,37 @@
 <div class="">
-    <div class="card-body">
+    <div class="card-body table-responsive">
 
-        <table class="table table-bordered">
+        <table class="table table-bordered table-hover">
             <tbody>
                 <tr>
                     <th>Patient Name</th>
-                    <td id="view-patient-name">Jan Louie Salimbago</td>
+                    <td class="text-wrap" id="view-patient-name">Jan Louie Salimbago</td>
                 </tr>
                 <tr>
                     <th>Administered By</th>
-                    <td>Nurse Joy</td>
+                    <td class="text-wrap">Nurse Joy</td>
                 </tr>
                 <tr>
                     <th>Date of Vaccination</th>
-                    <td id="view-date-of-vaccination">2025-06-25</td> <!-- Replace with dynamic value -->
+                    <td class="text-wrap" id="view-date-of-vaccination">2025-06-25</td> <!-- Replace with dynamic value -->
                 </tr>
                 <tr>
                     <th>Time of Vaccination</th>
-                    <td id="view-time-of-vaccination">09:00 AM</td> <!-- Replace with dynamic value -->
+                    <td class="text-wrap" id="view-time-of-vaccination">09:00 AM</td> <!-- Replace with dynamic value -->
                 </tr>
                 <tr>
                     <th>Vaccine Type</th>
-                    <td id="view-vaccine-type">
+                    <td class="text-wrap" id="view-vaccine-type">
                         Penta
                     </td>
                 </tr>
                 <tr>
                     <th>Vaccine Dose Number</th>
-                    <td id="view-dose-number">1st Dose</td>
+                    <td class="text-wrap" id="view-dose-number">1st Dose</td>
                 </tr>
                 <tr>
                     <th>Remarks</th>
-                    <td id="view-case-remarks"> No complications after administration</td>
+                    <td class="text-wrap" id="view-case-remarks"> No complications after administration</td>
                 </tr>
             </tbody>
         </table>
