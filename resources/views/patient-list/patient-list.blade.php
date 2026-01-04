b023b51ce0462acf07ab282dc53a7cb71d0086ac
kerby Buan <buan.kerby4@gmail.com>
2026-01-05 01:44:30 +0800

    fixes in merging

--- resources/views/patient-list/patient-list.blade.php
+++ resources/views/patient-list/patient-list.blade.php
@@ -30,33 +30,7 @@
             <!-- the main content -->
             <!-- we use flex-grow-1 to take the remaining space of the right side -->
             <div class="flex-grow-1 overflow-x-auto">
-                <header class=" d-flex align-items-center px-3 ">
-                    <nav class="d-flex justify-content-between align-items-center w-100 ">
-                        <div class="left-side d-flex gap-2">
-                            <button class="btn hamburger d-lg-block fs-6 mx-1" id="toggleSidebar">
-                                <i class="fa-solid fa-bars fs-2"></i>
-                            </button>
-                            <h1 class="mb-0"> {{$page ?? Welcome}}</span></h1>
-                        </div>
-                        <div class="profile-con position-relative justify-content-space d-flex align-items-center gap-2" style="min-width: 150px;">
-                            <img src="{{ optional(Auth::user()->nurses)->profile_image
-                        ? asset(optional(Auth::user()->nurses)->profile_image)
-                        : (optional(Auth::user()->staff)->profile_image
-                            ? asset(optional(Auth::user()->staff)->profile_image)
-                            : asset('images/default_profile.png')) }}" alt="profile picture" class="profile-img" id="profile_img">
-                            <div class="username-n-role">
-                                <h5 class="mb-0">{{ optional(Auth::user()->nurses)->full_name
-                                                    ?? optional(Auth::user()->staff)->full_name
-                                                    ?? 'none'  }}</h5>
-                                <h6 class="mb-0 text-muted fw-light">{{Auth::user() -> role ?? 'none';}}</h6>
-                            </div>
-                            <div class="links position-absolute flex-column top-17 w-100 bg-white" id="links">
-                                <a href="{{ route('page.profile') }}" class="text-decoration-none text-black">view profile</a>
-                                <a href="{{route('logout')}}" class="text-decoration-none text-black">Logout</a>
-                            </div>
-                        </div>
-                    </nav>
-                </header>
+                @include('layout.header')
                 <main class=" overflow-y-auto max-h-[calc(100vh-100px)] px-md-3 px-0">
 
 
