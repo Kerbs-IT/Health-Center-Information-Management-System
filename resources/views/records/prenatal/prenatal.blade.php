2b7c0afc09c9e2ba34bc28c282e2aa6038b98034
Jan Louie Salimbago <salimbagojanlouie02@gmail.com>
2026-01-04 19:39:43 +0800

    records-prenatal-responsiveness

--- resources/views/records/prenatal/prenatal.blade.php
+++ resources/views/records/prenatal/prenatal.blade.php
@@ -18,13 +18,13 @@
     'resources/css/patient/record.css',
     'resources/js/record/record.js'])
 
-    <div class="vaccination vh-100 d-flex">
+    <div class="vaccination min-vh-100 d-flex">
         <aside>
             @include('layout.menuBar')
         </aside>
-        <div class="d-flex flex-grow-1 flex-column">
+        <div class="d-flex flex-grow-1 flex-column overflow-x-auto">
             @include('layout.header')
-            <main class="flex-column p-2">
+            <main class="flex-column p-2 px-md-4 px-1">
                 <h1>Prenatal</h1>
                 <!-- body part -->
                 <livewire:prenatal.records-table>
--- resources/views/records/prenatal/viewComponent/viewPregnancyPlan.blade.php
+++ resources/views/records/prenatal/viewComponent/viewPregnancyPlan.blade.php
@@ -1,66 +1,68 @@
-<table class="table table-bordered table-striped">
-    <thead class="table-secondary text-center table-header">
-        <tr>
-            <th>Question</th>
-            <th>Answer</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td class="fw-bold bg-light">Ako ay papaanakin ni</td>
-            <td id="midwife_name_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Plano kong manganak sa</td>
-            <td id="place_of_pregnancy_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Otorisado ng PhilHealth</td>
-            <td id="authorized_by_philhealth_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Tinatayang gagastusin (P)</td>
-            <td id="cost_of_pregnancy_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Paraan ng pagbabayad</td>
-            <td id="mode_of_payment_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Paraan ng pagbiyahe</td>
-            <td id="mode_of_transportation_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Taong magdadala sa hospital</td>
-            <td id="accompany_person_to_hospital_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Taong sasamahan sa panganganak</td>
-            <td id="accompany_person_through_pregnancy_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Taong mag-aalaga pagkatapos manganak</td>
-            <td id="care_person_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Magbibigay ng dugo kung kailangan</td>
-            <td id="blood_donor_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Sabihan kung may komplikasyon - Pangalan</td>
-            <td id="emergency_person_name_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Sabihan kung may komplikasyon - Tirahan</td>
-            <td id="emergency_person_residency_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Sabihan kung may komplikasyon - Telepono</td>
-            <td id="emergency_person_contact_number_value"></td>
-        </tr>
-        <tr>
-            <td class="fw-bold bg-light">Pangalan ng pasyente</td>
-            <td id="patient_name_value"></td>
-        </tr>
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-bordered table-striped">
+        <thead class="table-secondary text-center table-header">
+            <tr>
+                <th>Question</th>
+                <th>Answer</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="fw-bold bg-light">Ako ay papaanakin ni</td>
+                <td id="midwife_name_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Plano kong manganak sa</td>
+                <td id="place_of_pregnancy_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Otorisado ng PhilHealth</td>
+                <td id="authorized_by_philhealth_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Tinatayang gagastusin (P)</td>
+                <td id="cost_of_pregnancy_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Paraan ng pagbabayad</td>
+                <td id="mode_of_payment_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Paraan ng pagbiyahe</td>
+                <td id="mode_of_transportation_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Taong magdadala sa hospital</td>
+                <td id="accompany_person_to_hospital_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Taong sasamahan sa panganganak</td>
+                <td id="accompany_person_through_pregnancy_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Taong mag-aalaga pagkatapos manganak</td>
+                <td id="care_person_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Magbibigay ng dugo kung kailangan</td>
+                <td id="blood_donor_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Sabihan kung may komplikasyon - Pangalan</td>
+                <td id="emergency_person_name_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Sabihan kung may komplikasyon - Tirahan</td>
+                <td id="emergency_person_residency_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Sabihan kung may komplikasyon - Telepono</td>
+                <td id="emergency_person_contact_number_value"></td>
+            </tr>
+            <tr>
+                <td class="fw-bold bg-light">Pangalan ng pasyente</td>
+                <td id="patient_name_value"></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
