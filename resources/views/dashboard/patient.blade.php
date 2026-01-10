1cd2517838d2f7f1bf0150fc722ce4409f549edd
Jan Louie Salimbago <salimbagojanlouie02@gmail.com>
2026-01-10 15:02:00 +0800

    patient-responsiveness

--- resources/views/dashboard/patient.blade.php
+++ resources/views/dashboard/patient.blade.php
@@ -31,16 +31,16 @@
             <!-- we use flex-grow-1 to take the remaining space of the right side -->
             <div class="flex-grow-1 ">
                 @include('layout.header')
-                <main class=" mt-4 d-flex align-items-center flex-column justify-content-center flex-grow-1 py-2 px-5">
-                    <div class="top-content d-flex w-100  shadow mb-5 rounded b patient-profile">
+                <main class=" mt-md-4 mt-2 d-flex align-items-center flex-column justify-content-center flex-grow-1 py-2 px-lg-5 px-md-3 px-2 overflow-x-auto">
+                    <div class="top-content d-flex w-100  shadow mb-5 rounded b patient-profile flex-lg-row flex-column">
                         <!-- Left panel -->
-                        <div class="edit-profile p-3 d-flex w-25 flex-column align-items-center border-end">
-                            <img src="{{ 
+                        <div class="edit-profile p-3 d-flex w-[100%] lg:w-[25%] flex-column align-items-center border-lg-end border-bottom">
+                            <img src="{{
     asset(
         optional(Auth::user())->profile_image ??
-        optional(Auth::user()->patient)->profile_image 
+        optional(Auth::user()->patient)->profile_image
         ?? 'images/default_profile.png'
-    ) 
+    )
 }}"
                                 alt="profile_img" class="mb-3 profile-section-image" style="width: 100px; height: 100px; object-fit: cover;">
                             <h4 class="mb-3">{{ optional(Auth::user())->username ?? 'none' }}</h4>
@@ -52,16 +52,18 @@
                         <div class="personal-info p-4 flex-grow-1">
                             <!-- Personal Information Section -->
                             <div class="info mb-4">
-                                <div class="d-flex w-100 justify-content-between mb-3">
+                                <div class="d-flex w-100 justify-content-between mb-3 flex-wrap">
                                     <h4 class="mb-0 pb-2 border-bottom fw-bold no-wrap flex-1">Personal Information</h4>
-                                    <div class="change-pass-button w-100 d-flex justify-content-end px-4 flex-1">
-                                        <a href="{{ route('change-pass') }}" class="btn btn-success">Change Password</a>
+                                    <div class="change-pass-button w-100 d-flex justify-content-end px-4 flex-1 mt-md-0 mt-1">
+                                        <div>
+                                          <a href="{{ route('change-pass') }}" class="btn btn-success text-nowrap">Change Password</a>
+                                        </div>
                                     </div>
                                 </div>
 
-                                <div class="row px-3">
+                                <div class="row px-md-3 px-1">
                                     <!-- Left Column -->
-                                    <div class="col-md-6">
+                                    <div class="col-sm-6">
                                         <div class="info-item mb-3">
                                             <label class="text-muted small mb-1">Name:</label>
                                             <p class="mb-0 fw-medium">{{ optional(Auth::user()->patient)->full_name ?? trim((Auth::user()->first_name ?? '') . ' ' . (Auth::user()->middle_initial ?? '') . ' ' . (Auth::user()->last_name ?? '')) ?: 'Not specified' }}</p>
@@ -85,7 +87,7 @@
                                     </div>
 
                                     <!-- Right Column -->
-                                    <div class="col-md-6">
+                                    <div class="col-sm-6">
                                         <div class="info-item mb-3">
                                             <label class="text-muted small mb-1">Nationality</label>
                                             <p class="mb-0 fw-medium">{{ optional(Auth::user()->patient)->nationality ?? 'Not specified' }}</p>
@@ -238,7 +240,7 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" style="filter: invert(1);"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="pop-up  w-100 h-100 d-flex align-items-start justify-content-center px-3 gap-3 mt-2" id="pop-up">
+                        <div class="pop-up  w-100 h-100 d-flex align-items-start justify-content-center px-3 gap-3 mt-2 flex-lg-nowrap flex-wrap" id="pop-up">
                             <!-- type of patient if available -->
                             <input type="hidden" name="typeOfPatient" value="{{$typeOfPatient??null}}">
 
@@ -257,36 +259,36 @@
                             <!-- USER INFORMATION -->
                             <div class="user-info flex-grow-1 ">
                                 <h4 class="fw-bold">Personal Info</h4>
-                                <div class="mb-2 d-flex gap-1">
-                                    <div class="input-field w-50">
+                                <div class="mb-2 d-flex gap-1 flex-xl-nowrap flex-wrap">
+                                    <div class="input-field flex-fill xl: w-[50%]">
                                         <input type="text" id="first_name" placeholder="First Name" class="form-control" name="first_name" value="">
                                         <small class="text-danger" id="fname-error"></small>
                                     </div>
-                                    <div class="input-field w-50">
+                                    <div class="input-field flex-fill xl: w-[50%]">
                                         <input type="text" id="middle_initial" placeholder="Middle Initial" class="form-control" name="middle_initial" value="">
                                         <small class="text-danger" id="middle-initial-error"></small>
                                     </div>
-                                    <div class="input-field w-50">
+                                    <div class="input-field flex-fill xl: w-[50%]">
                                         <input type="text" id="last_name" placeholder="Last Name" class="form-control" name="last_name" value="">
                                         <small class="text-danger" id="lname-error"></small>
                                     </div>
                                 </div>
                                 <!-- age -->
-                                <div class="mb-2 d-flex gap-1">
+                                <div class="mb-2 d-flex gap-1 flex-xl-nowrap flex-wrap ">
                                     @if($typeOfPatient)
-                                    <div class="input-field flex-1">
+                                    <div class="input-field flex-1 flex-fill xl:w-[50%]">
                                         <label for="age">Age</label>
                                         <input type="text" id="age" placeholder="20" class="form-control" name="age" value="">
                                         <small class="text-danger" id="age-error"></small>
                                     </div>
                                     @endif
-                                    <div class="input-field flex-1">
+                                    <div class="input-field flex-1 flex-fill xl:w-[50%]">
                                         <label for="birthdate">Date of Birth</label>
                                         <input type="date" id="birthdate" placeholder="20" class="form-control w-100 px-5" name="date_of_birth" value="">
                                         <small class="text-danger" id="birthdate-error"></small>
                                     </div>
                                     @if($typeOfPatient)
-                                    <div class="input-field flex-1">
+                                    <div class="input-field flex-1 flex-fill xl:w-[50%]">
                                         <label for="sex">Sex</label>
                                         <div class="input-field d-flex align-items-center p-2">
                                             @php
@@ -304,9 +306,9 @@
                                     @endif
                                 </div>
                                 <!-- civil status, contact number, nationality -->
-                                <div class="mb-2 d-flex gap-1">
+                                <div class="mb-2 d-flex gap-1 flex-xl-nowrap flex-wrap">
                                     @if($typeOfPatient)
-                                    <div class="input-field flex-1">
+                                    <div class="input-field flex-1 flex-fill xl:w-[50%]">
                                         <label for="civil_status" class="">Civil Status</label>
                                         <!-- to display the current status -->
 
@@ -319,34 +321,34 @@
                                     </div>
                                     @endif
                                     <!-- contact -->
-                                    <div class="input-field flex-1">
+                                    <div class="input-field flex-1 flex-fill xl:w-[50%]">
                                         <label for="contact_number" class="">Contact Number</label>
                                         <input type="number" placeholder="+63-936-627-8671" class="form-control" id="contact_num" name="contact_number" value="">
                                         <small class="text-danger" id="contact-error"></small>
                                     </div>
                                     @if($typeOfPatient)
-                                    <div class="input-field flex-1">
+                                    <div class="input-field flex-1 flex-fill xl:w-[50%]">
                                         <label for="nationality" class="">Nationality</label>
                                         <input type="text" placeholder="ex. Filipino" class="form-control" id="nationality" name="nationality" value="">
                                         <small class="text-danger" id="nationality-error"></small>
                                     </div>
                                     @endif
                                 </div>
-                                <div class="mb-2 d-flex gap-1">
+                                <div class="mb-2 d-flex gap-1 flex-xl-nowrap flex-wrap">
                                     <!-- username -->
-                                    <div class="input-field w-50">
+                                    <div class="input-field flex-fill xl: w-[50%]">
                                         <label for="username" class="">Username</label>
                                         <input type="text" placeholder="ex. yato" id="username" class="form-control" name="username" value="">
                                         <small class="text-danger" id="username-error"></small>
                                     </div>
                                     <!-- email -->
-                                    <div class="input-field w-50">
+                                    <div class="input-field flex-fill xl: w-[50%]">
                                         <label for="email" class="">Email</label>
                                         <input type="email" placeholder="ex. yato" id="email" class="form-control" name="email" value="">
                                         <small class="text-danger" id="email-error"></small>
                                     </div>
                                     <!-- password -->
-                                    <div class="input-field w-50">
+                                    <div class="input-field flex-fill xl: w-[50%]">
                                         <label for="password" class="">Password</label>
                                         <input type="password" id="edit_password" class="form-control" name="password">
                                         <small class="text-muted">Leave blank if you don't want to change it.</small>
@@ -357,15 +359,15 @@
                                 <div class="mb-3 w-100" id="patient_type_con">
                                     <label for="patient_type" class="form-label text-nowrap ">Patient Address </label>
 
-                                    <div class=" w-100 d-flex gap-2">
-                                        <div class="items w-50">
+                                    <div class=" w-100 d-flex gap-2 flex-xl-row flex-column flex-xl-nowrap flex-wrap">
+                                        <div class="items w-[100%] xl:[50%]">
                                             <label for="patient_street" class="w-100 text-muted">Blk & lot,Street*</label>
                                             <input type="text" id="update_blk_n_street" name="blk_n_street" placeholder="enter the blk & lot & street seperated by ','" class="w-100 form-control">
                                             @error('blk_n_street')
                                             <small class="text-danger">{{$message}}</small>
                                             @enderror
                                         </div>
-                                        <div class="items w-50">
+                                        <div class="items w-[100%] xl:[50%]">
                                             <label for="patient_purok_dropdown">Puroks*</label>
                                             <select id="update_patient_purok_dropdown" class="form-select w-100" name="patient_purok_dropdown" required>
                                                 <option value="" selected disabled>Select a purok</option>
@@ -409,14 +411,14 @@
                                         </div>
                                     </div>
                                     @elseif(($typeOfPatient ?? null) === 'prenatal')
-                                    <div class="mb-2 d-flex gap-2">
-                                        <div class="input-field flex-1">
+                                    <div class="mb-2 d-flex gap-2 flex-wrap flex-xl-nowrap">
+                                        <div class="input-field flex-1 flex-fill flex-xl:w-[50%]">
                                             <label for="motherName">Head of the Family</label>
                                             <input type="text" id="head_of_the_family" placeholder="Enter the Name" class="form-control" name="family_head_name" value="">
                                             <small class="text-danger error-text" id="family_head_name_error"></small>
 
                                         </div>
-                                        <div class="input-field flex-1">
+                                        <div class="input-field flex-1 flex-fill flex-xl:w-[50%]">
                                             <label for="blood_type">Blood Type</label>
                                             <select name="blood_type" id="blood_type" class="form-select" required>
                                                 <option value="" disabled selected>Select Blood Type</option>
@@ -431,7 +433,7 @@
                                             </select>
                                             <small class="text-danger error-text" id="blood_type_error"></small>
                                         </div>
-                                        <div class="input-field flex-1">
+                                        <div class="input-field flex-1 flex-fill flex-xl:w-[50%]">
                                             <label for="religion" class="form-label">Religion</label>
                                             <input type="text" id="religion" placeholder="Enter the Religion" class="form-control" name="religion" value="">
                                             <!-- ERROR HANDLING -->
@@ -520,7 +522,6 @@
                                 </div>
 
                             </div>
-
                         </div>
                     </div>
                     <div class="modal-footer d-flex justify-content-end gap-2">
