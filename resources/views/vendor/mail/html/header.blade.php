47a413d639661ecd4f606761b99ca400201540f2
kerby Buan <buan.kerby4@gmail.com>
2026-05-05 04:30:23 +0800

    fix the logo in email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,12 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @php
-            $logoPath = public_path('images/hugo_perez_logo.png');
-            $logoData = base64_encode(file_get_contents($logoPath));
-            $logoSrc = 'data:image/png;base64,' . $logoData;
-            @endphp
-            <img src="{{ $logoSrc }}"
+            <img src="https://hugoperezproperbhc.com/images/hugo_perez_logo.png"
                 alt="Health Center IMS"
                 style="width: 60px; height: 60px; border-radius: 50%;">
         </a>
